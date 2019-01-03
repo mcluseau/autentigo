d16c0c1adbddb19b83a5b967548d5d693d0b9cbc
blancinot <blancinot@isi.nc>
2019-01-03 15:42:52 +1100

    chore(companion-api): Dockerfile + go vendoring

---
+++ companion-api/api/errors.go
@@ -0,0 +1,27 @@
+package api
+
+import (
+	"log"
+	"net/http"
+	"runtime"
+
+	"github.com/emicklei/go-restful"
+)
+
+// write error in good http format with error stack in it
+func writeError(err error, response *restful.Response) {
+	response.AddHeader("Content-Type", "text/plain")
+
+	if rfErr, ok := err.(restful.ServiceError); ok {
+		response.WriteError(rfErr.Code, rfErr)
+		return
+	}
+
+	ba := make([]byte, 10240)
+	n := runtime.Stack(ba, false)
+	log.Print("error during request: ", err, "\n", string(ba[:n]))
+
+	status := http.StatusInternalServerError
+
+	response.WriteErrorString(status, http.StatusText(status)+"\n")
+}
